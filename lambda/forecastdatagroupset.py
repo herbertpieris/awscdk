250d7c39267d79711cd82f3f4b50511757167f44
herbert <herbert.pieris@japfa.com>
2022-05-18 10:27:55 +0700

    create forecast dataset group

--- lambda/forecastdatagroupset.py
+++ lambda/forecastdatagroupset.py
@@ -2,7 +2,7 @@ import json
 import boto3
 
 def lambda_handler(event, context):
-  forecast = boto3.client(forecast)
+  forecast = boto3.client('forecast')
   response = forecast.create_dataset_group(
     DatasetGroupName='hapcustforecast',
     Domain='INVENTORY_PLANNING'
